a501605735abd0148f626db673499b8afc8095f1
alexgl <nemofulcrum@gmail.com>
2012-12-11 02:25:42 -0600

    about to start on demo ads

--- sites/all/themes/hub_zen/templates/page.tpl.php
+++ sites/all/themes/hub_zen/templates/page.tpl.php
@@ -76,7 +76,7 @@
 
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-      <img src="<?php print ($directory . '/images/masthead_logo.png'); ?>" style="margin-left:100px;">
+      <img src="<?php print ($base_path . drupal_get_path('theme','hub_zen') . '/images/masthead_logo.png'); ?>" style="margin-left:100px;margin-bottom:-10px;">
     <?php endif; ?>
 
     
@@ -102,25 +102,6 @@
   </header>
 
   <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title && !drupal_is_front_page()): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div><!-- /#content -->
 
     <div id="navigation">
 
@@ -151,6 +132,29 @@
 
     </div><!-- /#navigation -->
 
+    
+    
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title && !drupal_is_front_page()): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div><!-- /#content -->
+
+    
+    
     <?php
       // Render the sidebars to see if there's anything in them.
       $sidebar_first  = render($page['sidebar_first']);
